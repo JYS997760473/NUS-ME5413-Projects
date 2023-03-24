371f549510e818b0bb8e13fac6e0bfdb155eda44
JYS997760473 <jiayansong@u.nus.edu>
2023-03-25 01:26:37 +0800

    for final project slam

---
+++ finalProject/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jiayansong/nus/ME5413/ME5413_Final_Project/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/jiayansong/nus/ME5413/ME5413_Final_Project/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jiayansong/nus/ME5413/ME5413_Final_Project/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jiayansong/nus/ME5413/ME5413_Final_Project/src:$ROS_PACKAGE_PATH"
